9aa586511882d74ee39fed9cd6f5cfab516ce2b3
mendodev <mendodevcs@gmail.com>
2024-05-27 12:01:42 -0600

    Configured the environment, built the API request

---
+++ src/main/java/com/aluracursos/desafio_literalura/DesafioLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiteraluraApplication.class, args);
+	}
+
+}
