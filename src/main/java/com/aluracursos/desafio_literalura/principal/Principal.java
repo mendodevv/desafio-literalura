b9a6e61c4f9a3ef540b2d5c07dec16df213a2e30
mendodevv <mendodevcs@gmail.com>
2024-05-31 18:42:55 -0600

    Added obtain stadistics function and bugfixes

--- src/main/java/com/aluracursos/desafio_literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio_literalura/principal/Principal.java
@@ -33,8 +33,8 @@ public class Principal {
                     4 - Listar autores vivos en un determinado año
                     5 - Listar libros por idioma
                     6 - Top 10 libros más descargados
+                    7 - Obtener estadísiticas
                     0 - Salir
-                  
                     """;
             System.out.println(menu);
             opcion = teclado.nextInt();
@@ -58,6 +58,9 @@ public class Principal {
                     break;
                 case 6:
                     topDiezLibros();
+                    break;
+                case 7:
+                    estaditicasApi();
                     break;
                     case 0:
                         System.out.println("Cerrando aplicación...");
@@ -181,18 +184,75 @@ public class Principal {
     }
 
     private void topDiezLibros() {
-        var json = consumoApi.obtenerDatos(URL_BASE);
-        Datos datos = conversor.obtenerDatos(json, Datos.class);
-        List<Libros> libros = new ArrayList<>();
-        for (DatosLibros datosLibros : datos.resultados()) {
-            Autores autor = new Autores(datosLibros.autor().get(0));
-            Libros libro = new Libros(datosLibros, autor);
-            libros.add(libro);
+        System.out.println("De donde quieres obtener los libros más descargados?");
+        String menu = """
+                1 - Gutendex
+                2 - Base de datos
+                """;
+        System.out.println(menu);
+        var opcion = teclado.nextInt();
+        teclado.nextLine();
+
+        if (opcion == 1) {
+            var json = consumoApi.obtenerDatos(URL_BASE);
+            Datos datos = conversor.obtenerDatos(json, Datos.class);
+            List<Libros> libros = new ArrayList<>();
+            for (DatosLibros datosLibros : datos.resultados()) {
+                Autores autor = new Autores(datosLibros.autor().get(0));
+                Libros libro = new Libros(datosLibros, autor);
+                libros.add(libro);
+            }
+            libros.stream()
+                    .sorted(Comparator.comparing(Libros::getNumeroDescargas).reversed())
+                    .limit(10)
+                    .forEach(System.out::println);
+        } else if (opcion == 2) {
+            List<Libros> libros = librosRepository.findAll();
+            if (libros.isEmpty()) {
+                System.out.println("No hay libros registrados");
+                return;
+            }
+            libros.stream()
+                    .sorted(Comparator.comparing(Libros::getNumeroDescargas).reversed())
+                    .limit(10)
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("Opción no válida, intenta de nuevo");
         }
-        libros.stream()
-            .sorted(Comparator.comparing(Libros::getNumeroDescargas).reversed())
-            .limit(10)
-            .forEach(System.out::println);
+    }
+
+    private void estaditicasApi() {
+        System.out.println("De donde quieres obtener las estadísiticas)");
+        String menu = """
+                1 - Gutendex
+                2 - Base de datos
+                """;
+        System.out.println(menu);
+        var opcion = teclado.nextInt();
+        teclado.nextLine();
+
+        if (opcion == 1) {
+            var json = consumoApi.obtenerDatos(URL_BASE);
+            Datos datos = conversor.obtenerDatos(json, Datos.class);
+            DoubleSummaryStatistics estadisticas = datos.resultados().stream()
+                    .collect(Collectors.summarizingDouble(DatosLibros::numeroDescargas));
+            System.out.println("Libro con más descargas: " + estadisticas.getMax());
+            System.out.println("Libro con menos descargas: " + estadisticas.getMin());
+            System.out.println("Promedio de descargas: " + estadisticas.getAverage());
+        } else if (opcion == 2) {
+            List<Libros> libros = librosRepository.findAll();
+            if (libros.isEmpty()) {
+                System.out.println("No hay libros registrados");
+                return;
+            }
+            DoubleSummaryStatistics estadisticas = libros.stream()
+                    .collect(Collectors.summarizingDouble(Libros::getNumeroDescargas));
+            System.out.println("Libro con más descargas: " + estadisticas.getMax());
+            System.out.println("Libro con menos descargas: " + estadisticas.getMin());
+            System.out.println("Promedio de descargas: " + estadisticas.getAverage());
+        } else {
+            System.out.println("Opción no válida, intenta de nuevo");
+        }
     }
 }
 
